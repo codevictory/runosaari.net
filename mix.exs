cd46df9c57ef278a2ae9fc7fb916301362d56d8d
codevictory <main@codevictory.net>
2021-03-29 21:46:38 +0300

    Dialyxir and credo installations.

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Runosaari.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_deps: :transitive]
     ]
   end
 
@@ -44,7 +45,9 @@ defmodule Runosaari.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
