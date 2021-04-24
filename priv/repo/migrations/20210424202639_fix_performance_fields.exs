210dbc7bc89e26e425ad99d8855c228381c8cba7
codevictory <main@codevictory.net>
2021-04-24 23:52:51 +0300

    Migration: Fix performances fields.

---
+++ priv/repo/migrations/20210424202639_fix_performance_fields.exs
@@ -0,0 +1,21 @@
+defmodule Runosaari.Repo.Migrations.FixPerformanceFields do
+  use Ecto.Migration
+
+  def up do
+    alter table("performances") do
+      remove :notes
+      remove :time
+      remove :description
+      add :description, :string, size: 2000
+      add :name, :string
+    end
+  end
+
+  def down do
+    alter table("performances") do
+      add :notes, :string
+      add :time, :naive_datetime
+      remove :name
+    end
+  end
+end
