4d50936d49b352c341adde93fb16649a21112ac9
codevictory <main@codevictory.net>
2021-04-24 16:50:47 +0300

    Change performer field usage according to the changes in schema.

--- lib/runosaari/registration/performer.ex
+++ lib/runosaari/registration/performer.ex
@@ -7,8 +7,13 @@ defmodule Runosaari.Registration.Performer do
     field :email, :string
     field :fname, :string
     field :lname, :string
-    field :notes, :string
+    field :desc, :string
     field :tel, :string
+    field :date1, :boolean, default: false
+    field :date2, :boolean, default: false
+    field :date3, :boolean, default: false
+    field :bus, :boolean, default: false
+    field :accom, :boolean, default: false
 
     timestamps()
   end
@@ -16,8 +21,32 @@ defmodule Runosaari.Registration.Performer do
   @doc false
   def changeset(performer, attrs) do
     performer
-    |> cast(attrs, [:fname, :lname, :email, :tel, :confirmed, :notes])
-    |> validate_required([:fname, :lname, :email, :tel, :confirmed, :notes])
+    |> cast(attrs, [
+      :fname,
+      :lname,
+      :email,
+      :tel,
+      :confirmed,
+      :desc,
+      :date1,
+      :date2,
+      :date3,
+      :bus,
+      :accom
+    ])
+    |> validate_required([
+      :fname,
+      :lname,
+      :email,
+      :tel,
+      :confirmed,
+      :desc,
+      :date1,
+      :date2,
+      :date3,
+      :bus,
+      :accom
+    ])
     |> unique_constraint(:email)
   end
 end
