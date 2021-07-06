af2c4f681ff6bcd5c6e8b51fbc42be493dd7c544
codevictory <main@codevictory.net>
2021-07-06 20:03:12 +0300

    Remove unused page controller endpoint.

--- lib/runosaari_web/controllers/page_controller.ex
+++ lib/runosaari_web/controllers/page_controller.ex
@@ -1,10 +1,6 @@
 defmodule RunosaariWeb.PageController do
   use RunosaariWeb, :controller
 
-  def info(conn, _params) do
-    render(conn, "info.html")
-  end
-
   def covid19(conn, _params) do
     render(conn, "covid19.html")
   end
